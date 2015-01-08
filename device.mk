d515aff4fd2e0d7e70ba37ebc35a48a02778b914
CPA-Poke <brock.shahan@cox.net>
2015-01-07 22:36:22 -0500

    Update ramdisk
    * Add franco performance profiles
    
    Change-Id: Iaeba828a5e9225b778951b1d7835422a1a9a5328
    
    Conflicts:
    	device.mk
    	init.shamu.rc

--- device.mk
+++ device.mk
@@ -25,7 +25,8 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/init.shamu.power.rc:root/init.shamu.power.rc \
     device/moto/shamu/init.shamu.usb.rc:root/init.shamu.usb.rc \
     device/moto/shamu/fstab.shamu:root/fstab.shamu \
-    device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc
+    device/moto/shamu/ueventd.shamu.rc:root/ueventd.shamu.rc \
+    device/motorola/shamu/init.performance_profiles.rc:root/init.performance_profiles.rc
 
 # Input device files for shamu
 PRODUCT_COPY_FILES += \
